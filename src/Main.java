247aafc95f6984c94750603dbd7fb0c4bc218a13
Ed White <ew@spyhi.com>
2018-09-26 21:43:40 -1000

    Added listview for filenames and filtered files now saved
    Can now view filenames of files that were selected and the program now writes the filtered version of the selected images to "./testimg/filtered/"+image.toString()+".jpg"

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
@@ -12,11 +11,16 @@ import javafx.stage.Stage;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOError;
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.embed.swing.SwingFXUtils;
 
 import com.jhlabs.image.InvertFilter;
+
+import javax.imageio.ImageIO;
 
 public class Main extends Application {
 
@@ -34,41 +38,56 @@ public class Main extends Application {
 
 
     button = new Button("Choose File");
+    ListView imageList = new ListView();
+
+
     ImageView iv = new ImageView();
-    Label fileLabel = new Label("File Name Here");
+    InvertFilter filter = new InvertFilter();
+
+    FileChooser fileChooser = new FileChooser();
+    fileChooser.setTitle("Open Image File");
+    fileChooser.getExtensionFilters().addAll(
+        new ExtensionFilter("Image Files", "*.jpg"));
 
     button.setOnAction(e -> {
-      Image image;
-      InvertFilter filter = new InvertFilter();
-      FileChooser fileChooser = new FileChooser();
-      fileChooser.setTitle("Open Image File");
-      fileChooser.getExtensionFilters().addAll(
-          new ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif"));
-      File selectedFile = fileChooser.showOpenDialog(window);
+      List<Image> images = new ArrayList<>();
+      List<File> selectedFiles = fileChooser.showOpenMultipleDialog(window);
 
-      if (selectedFile != null) {
-        fileLabel.setText(selectedFile.getName());
-        try {
-          image = new Image(selectedFile.toURI().toURL().toString());
-          image = SwingFXUtils.toFXImage(
-              filter.filter(
-                  SwingFXUtils.fromFXImage(
-                      image, null), null), null);
-          iv.setImage(image);
-          iv.setFitWidth(250);
-          iv.setPreserveRatio(true);
-        } catch (MalformedURLException x) {
-          System.out.println("To URL failed");
-        }
+      if (selectedFiles != null) {
+        selectedFiles.forEach(file -> {
+          imageList.getItems().add(file.toString());
+          try {
+            images.add(new Image(file.toURI().toURL().toString()));
+          } catch (MalformedURLException x) {
+            System.out.println("To URL failed");
+          }
+        });
+
+        images.forEach(image -> { //SwingFXUtils.toFXImage(
+          BufferedImage img = filter.filter(SwingFXUtils.fromFXImage(image, null), null); // , null);
+          String fmt = "jpg";
+          File imgFilepath = new File("./testimg/filtered/"+image.toString()+".jpg");
+          try {
+            ImageIO.write(img, fmt, imgFilepath);
+          } catch (IOException ioex) {
+            System.out.println("Image file write failed");
+          }
+
+        });
+
+
+        iv.setImage(images.get(1));
+        iv.setFitWidth(1000);
+        iv.setPreserveRatio(true);
       }
     });
 
 
     VBox layout = new VBox();
     layout.getChildren().add(button);
-    layout.getChildren().add(fileLabel);
+    layout.getChildren().add(imageList);
     layout.getChildren().add(iv);
-    Scene scene = new Scene(layout, 300, 300);
+    Scene scene = new Scene(layout, 1000, 800);
     window.setScene(scene);
     window.show();
 
